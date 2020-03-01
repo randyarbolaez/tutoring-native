6b413d5616275e5d399a9884682092dc653b69bc
Randy <randyarbolaez0@gmail.com>
2020-03-01 10:25:08 -0500

    changed  splash & icon to something cool || artist credit in App.js

--- App.js
+++ App.js
@@ -23,3 +23,5 @@ const App = () => {
 };
 
 export default App;
+
+// <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
